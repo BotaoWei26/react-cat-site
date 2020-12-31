a5f318c5c096e90c273b3c8450355794ef86526f
Botao Wei <b26wei@uwaterloo.ca>
2020-12-30 22:32:53 -0500

    added random button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Input } from 'semantic-ui-react'
+import { Input, Button, Icon } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 import Quote from './Quote'
 import Image from './Image'
@@ -9,8 +9,8 @@ const CAT_QUOTE_URL = "https://cat-fact.herokuapp.com/facts";
 const DEFAULT_TAG = "cute";
 
 function App() {
-  const [quote, setQuote] = useState("")
-  const [tag, setTag] = useState(DEFAULT_TAG)
+  const [quote, setQuote] = useState("");
+  const [tag, setTag] = useState(DEFAULT_TAG);
 
   useEffect(() => {
     async function fetchData() {
@@ -27,10 +27,20 @@ function App() {
     const value = e.target.value;
     setTag(value === "" ? DEFAULT_TAG : value);
   };
+  
+  const handleRandomButton = e => {
+    setTag("");
+  };
 
   return (
     <div className="App">
-      <Image tag={tag}/>
+      <Image tag={tag} />
+      <Button animated onClick={handleRandomButton}>
+        <Button.Content visible>Random</Button.Content>
+        <Button.Content hidden>
+          <Icon name="random" />
+        </Button.Content>
+      </Button>
       <Input
         placeholder={DEFAULT_TAG}
         onChange={handleInputChange}
--- src/Image.js
+++ src/Image.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import "./Image.css"
 
 const CAT_IMAGE_URL = "https://cataas.com/cat";
 
 function Image({ tag }) {
+
+    const getImage = (() => {
+        return <img
+            src={`${CAT_IMAGE_URL}${tag ? "/" + tag : ""}`}
+            alt={ `No cats like ${tag}` }
+        />  
+    });
+    
     return (
-        <div>
-            <img src={`${CAT_IMAGE_URL}/${tag}`} alt=""/>
+        <div className="container">
+            {getImage()}
         </div>
     )
 }
